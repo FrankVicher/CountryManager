aa95d530b50c14befbde8a33896f464eb5f11de1
Francisco Vicher Martinez (CONTRACTOR) <francisco.vicherma@laureate.mx>
2020-08-06 09:42:04 -0500

    Se agrega swagger

--- CountryManager/CountryManager/Startup.cs
+++ CountryManager/CountryManager/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CountryManager
 {
@@ -25,7 +26,12 @@ namespace CountryManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
             services.AddControllers();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +41,7 @@ namespace CountryManager
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -46,6 +52,13 @@ namespace CountryManager
             {
                 endpoints.MapControllers();
             });
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
         }
     }
 }
